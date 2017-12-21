41a4499b52140d4ee159a3023906b7a0f8198018
Manuel Iglesias <manuelig@amazon.com>
2017-12-21 15:05:08 -0700

    Upgrade react-native

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-
-AppRegistry.registerComponent('events', () => App);
+AppRegistry.registerComponent('awsappsyncevents', () => App);
